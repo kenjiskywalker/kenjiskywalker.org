4cffdb854316a74f63021980911844add2f2f4fd
kenjiskywalker <git@kenjiskywalker.org>
2016-11-08 00:09:39 +0900

    😀

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ git subtree pull --prefix=public git@github.com:kenjiskywalker/kenjiskywalker.or
 rm -rf ./public/
 hugo
 mv ./public/sitemap.xml ./public/atom.xml
-cp ./CNAME ./public/CNAME
+# cp ./CNAME ./public/CNAME
 # git subtree push --prefix=public git@github.com:kenjiskywalker/kenjiskywalker.org.git gh-pages
